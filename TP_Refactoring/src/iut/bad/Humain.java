4b3b63d7388c9ecfa81204c4d4dd0f32d248a377
Sanya <sanyaranoelisoa@gmail.com>
2024-03-27 23:12:41 -0700

    ajout de la methode détail dans Humain

--- TP_Refactoring/src/iut/bad/Humain.java
+++ TP_Refactoring/src/iut/bad/Humain.java
@@ -10,5 +10,15 @@ public class Humain {
 	  this.prenom = prenom;
 	  this.age = age;
 	}
+	
+	//methode pour afficher le nom, prénom, age en utilisant un méthode toString
+	public void details() {
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age;
+    }    
 
 }
