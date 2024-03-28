1343e433d0f8ade150af93f0d0eb624f68045b5a
Sanya <sanyaranoelisoa@gmail.com>
2024-03-27 23:27:30 -0700

    instanciation de homme/femme dans la classe Femme

--- TP_Refactoring/src/iut/bad/Femme.java
+++ TP_Refactoring/src/iut/bad/Femme.java
@@ -1,8 +1,25 @@
 package iut.bad;
 
-public class Femme extends Humain{
+public class Femme extends Humain implements Consommation{
 	
 	public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
+    }
+	
+	@Override
+    public void manger() {
+        System.out.println("La femme mange.");
+    }
+
+    @Override
+    public void boire() {
+        System.out.println("La femme boit.");
+    }
+
+    public static void main(String[] args) {
+        Homme homme = new Homme("Jean", "Dupont", 30);
+        Femme femme = new Femme("Marie", "Dupont", 25);
+
+        homme.ami(femme);
     }
 }
--- TP_Refactoring/src/iut/bad/Homme.java
+++ TP_Refactoring/src/iut/bad/Homme.java
@@ -1,8 +1,18 @@
 package iut.bad;
 
-public class Homme extends Humain{
+public class Homme extends Humain implements Consommation{
 	
 	public Homme(String nom, String prenom, int age) {
         super(nom, prenom, age);
+    }
+	
+	@Override
+    public void manger() {
+        System.out.println("L'homme mange.");
+    }
+
+    @Override
+    public void boire() {
+        System.out.println("L'homme boit.");
     }
 }
